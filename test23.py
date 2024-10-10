347af7f3f1b90015d30e08f19996cfa33e9babce
GIRI PRASAD P <giriprasa@gmail.com>
2024-10-10 17:21:04 +0530

    function creation

---
+++ test23.py
@@ -0,0 +1,14 @@
+#FUNCTION IN PYTHON
+# SET OF CODE 
+# DEFINE -> DEF  FUNCTION NAME -> PARAMETER -> CODE -> RETURN
+# CALL  -> FUNCTION NAME -> ARGUMENT -> RETURN  
+
+def welcome(fname, lname):
+    print("Thank you for joining us, " + fname + " " + lname)
+
+welcome("Giri","Prasad")
+welcome("John","Doe")
+welcome("Prabhakar","Vel")
+welcome("Michael", "Asif")
+welcome("Mohammed","Arafat")
+welcome("Steffen", "Friedrich")
