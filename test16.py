0f9181f14f6d29e828319b272aec3cfb30e79a8d
GIRI PRASAD P <giriprasa@gmail.com>
2024-10-10 15:42:08 +0530

    comparison operator

---
+++ test16.py
@@ -0,0 +1,17 @@
+x = 20
+y = 50
+
+if x == y:                  # check if x is equal to y
+    print("x is equal to y")
+elif x > y:                  # check if x is greater than y
+    print("x is greater than y")
+elif x >= y:                # check if x is greater than or equal to y
+    print("x is greater than or equal to y")
+elif x <= y:        # check if x is less than or equal to y
+    print("x is less than or equal to y")
+elif x != y:                # check if x is not equal to y
+    print("x is not equal to y")
+else:               
+    print("x is less than y")
+    
+    
