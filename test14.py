4a0d3a413119761a44017986efc69d3a33af9cdb
GIRI PRASAD P <giriprasa@gmail.com>
2024-10-10 15:26:13 +0530

    arithmetic operators

---
+++ test14.py
@@ -0,0 +1,35 @@
+#Operator
+'''
+    aritmetic operator  +, -, *, /, %, **, //
+    assignment operator =, +=, -=, *=, /=, %=, **=, //=
+    comparison operator ==, !=, >, <, >=, <=
+    logical operator and, or, not
+    bitwise operator &, |, ^, ~, <<, >>
+    identity operator is, is not
+    membership operator in, not in
+    
+'''
+x = 200
+y = 10
+
+result = x + y
+print(result)
+
+result = x - y
+print(result)
+
+result = x * y
+print(result)
+
+result = x / y
+print(result)
+
+result = x % y
+print(result)
+
+result = x ** y
+print(result)
+
+y = 19
+result = x // y
+print(result)
