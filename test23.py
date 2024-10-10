84fba0b7e6ca4a8d108762f1f01d09c35952988c
GIRI PRASAD P <giriprasa@gmail.com>
2024-10-10 17:27:22 +0530

    parameters and returns in function

--- test23.py
+++ test23.py
@@ -3,12 +3,24 @@
 # DEFINE -> DEF  FUNCTION NAME -> PARAMETER -> CODE -> RETURN
 # CALL  -> FUNCTION NAME -> ARGUMENT -> RETURN  
 
-def welcome(fname, lname):
-    print("Thank you for joining us, " + fname + " " + lname)
+def welcome(fname, lname, country = "Poland"):
+    print("Thank you for joining us, " + fname + " " + lname + " from " + country)
 
-welcome("Giri","Prasad")
-welcome("John","Doe")
-welcome("Prabhakar","Vel")
-welcome("Michael", "Asif")
-welcome("Mohammed","Arafat")
-welcome("Steffen", "Friedrich")
+welcome("Giri","Prasad", "India")
+welcome("John","Doe", "USA")
+welcome("Prabhakar","Vel", "India")
+welcome("Michael", "Asif", "USA")
+welcome("Mohammed","Arafat", "Bangladesh")
+welcome("Steffen", "Friedrich", "Germany")
+welcome("Sergio", "Ramos")
+
+def quadratic(a,b,c):
+    return b**2 - 4*a*c
+
+x = quadratic(1,2,1)
+y = quadratic(1,3,1)
+z = quadratic(1,4,1)
+
+print(x)
+print(y)
+print(z)
