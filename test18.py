174e3fde3c1660eda65f8fb83bd87ff58f0965cc
GIRI PRASAD P <giriprasa@gmail.com>
2024-10-10 15:57:17 +0530

    list 1

---
+++ test18.py
@@ -0,0 +1,18 @@
+"""
+    List
+    store multiple values in a single variable
+    myList = [1, 2, 3, 4, 5]
+"""
+    
+myList = [1, 2, 3, 3, 4, 4, 5]
+print(myList)   
+print(len(myList))  # length of the list    
+
+myList1 = ["apple", "banana", "cherry"]
+print(myList1)
+
+myList2 = [True, False, False, True]
+print(myList2)
+
+myList3 = [1, 2, 3, 4, "apple", "banana", "cherry", True, False]    
+print(myList3)
