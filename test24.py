604ba7d95387096423185622f5696b2055dea9f9
GIRI PRASAD P <giriprasa@gmail.com>
2024-10-10 17:31:24 +0530

    Lambda function

---
+++ test24.py
@@ -0,0 +1,11 @@
+sL = lambda : print("Welcom to Lambda function")
+sL()
+sL()
+
+quadratic = lambda a,b,c : b**2 - 4*a*c
+x = quadratic(1,2,1)
+y = quadratic(1,3,1)
+z = quadratic(1,4,1)
+print(x)
+print(y)
+print(z)
