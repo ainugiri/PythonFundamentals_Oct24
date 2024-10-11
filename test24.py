70f0c595f367c071521de971dca569380289b5d9
GIRI PRASAD P <giriprasa@gmail.com>
2024-10-11 15:17:13 +0530

    lambda updated

--- test24.py
+++ test24.py
@@ -1,11 +1,7 @@
-sL = lambda : print("Welcom to Lambda function")
-sL()
-sL()
-
-quadratic = lambda a,b,c : b**2 - 4*a*c
-x = quadratic(1,2,1)
-y = quadratic(1,3,1)
-z = quadratic(1,4,1)
-print(x)
-print(y)
-print(z)
+def myfun1(n):
+    return lambda a: a * n 
+                # a: 15, n: 5
+doublethevalue = myfun1(2)
+triplethevalue = myfun1(3)
+print(doublethevalue(11))
+print(triplethevalue(11))
