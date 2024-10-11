3c3dbc36d0ca958614b09a6267bfff7677f3c8d9
GIRI PRASAD P <giriprasa@gmail.com>
2024-10-11 16:58:39 +0530

    Math Module

---
+++ test31Math.py
@@ -0,0 +1,26 @@
+# built in math module
+import math
+
+y = min(200,201,200.5)
+print("minimum is ", y)
+
+
+y = max(200,201,200.5)
+print(y)
+
+x = abs(-7.25)
+print(x)
+
+x  = pow(9,3)
+print(x)
+
+x = math.sqrt(128)
+print(x)
+
+x = math.ceil(math.sqrt(128))
+print(x)
+
+x = math.floor(math.sqrt(128))
+print(x)
+
+print(math.pi)
